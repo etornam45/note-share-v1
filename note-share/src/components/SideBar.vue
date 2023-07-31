2e5d03ad5acdbbff5e565d32dc148778ba8af707
etornam45 <navidben45@gmail.com>
2023-07-31 13:16:09 +0000

    Added Sidebar

--- note-share/src/components/SideBar.vue
+++ note-share/src/components/SideBar.vue
@@ -1,123 +1,74 @@
 <template>
-  <div class="tree">
-    <TreeView :treeData="treeData" />
+  <div style="padding: 20px 10px;">
+    <tree-view
+      :tree-data="myTreeData"
+      @load-children="handleLoadChildren"
+    ></tree-view>
   </div>
 </template>
 
 <script>
+import { defineComponent, onMounted, reactive } from "vue";
 import TreeView from "./TreeView.vue";
+import PocketBase from "pocketbase";
+import { useUserStore } from "src/stores/user-store";
+import { useRouter } from "vue-router";
 
-export default {
+
+export default defineComponent({
   components: {
     TreeView,
   },
-  data() {
-    return {
-      treeData: [
-        {
-          id: 1,
-          label: "Node 1",
-          children: [
-            {
-              id: 2,
-              label: "Child 1",
-              children: [
-                {
-                  id: 4,
-                  label: "Grandchild 1",
-                  children: [],
-                },
-              ],
-            },
-            {
-              id: 3,
-              label: "Child 2",
-              children: [
-                {
-                  id: 4,
-                  label: "Grandchild 1",
-                  children: [
-                  {
-              id: 2,
-              label: "Child 1",
-              children: [
-                {
-                  id: 4,
-                  label: "Grandchild 1",
-                  children: [],
-                },
-              ],
-            },
-            {
-              id: 3,
-              label: "Child 2",
-              children: [
-                {
-                  id: 4,
-                  label: "Grandchild 1",
-                  children: [
-                  {
-              id: 2,
-              label: "Child 1",
-              children: [
-                {
-                  id: 4,
-                  label: "Grandchild 1",
-                  children: [ {
-              id: 2,
-              label: "Child 1",
-              children: [
-                {
-                  id: 4,
-                  label: "Grandchild 1",
-                  children: [],
-                },
-              ],
-            },
-            {
-              id: 3,
-              label: "Child 2",
-              children: [
-                {
-                  id: 4,
-                  label: "Grandchild 1",
-                  children: [],
-                },
-              ],
-            },],
-                },
-              ],
-            },
-            {
-              id: 3,
-              label: "Child 2",
-              children: [
-                {
-                  id: 4,
-                  label: "Grandchild 1",
-                  children: [],
-                },
-              ],
-            },
-                  ],
-                },
-              ],
-            },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    };
+  setup() {
+    const pb = new PocketBase("http://127.0.0.1:8090");
+    const userStore = useUserStore();
+    const router = useRouter()
+    const state = reactive({
+      myTreeData: [],
+    });
+
+    async function handleLoadChildren(item) {
+      await pb
+        .collection("noteTree")
+        .getFullList({
+          filter: `parent = '${item.id}'`,
+        })
+        .then((result) => {
+          result.forEach((node) => {
+            if (!item.children.some((child) => child.id == node.id)) {
+              item.children.push({
+                label: node.name,
+                id: node.id,
+                children: [],
+              });
+            }
+          });
+        }).catch(err => {
+            console.log(err)
+        }).finally(() => {
+            // handle routing here
+            router.push('/'+ item.id)
+        });
+    }
+
+    onMounted(async () => {
+      await pb
+        .collection("noteTree")
+        .getFullList({
+          filter: `user = '${userStore.user.id}' && parent = ''`,
+        })
+        .then((result) => {
+          result.forEach((node) => {
+            state.myTreeData.push({
+              label: node.name,
+              id: node.id,
+              children: [],
+            });
+          });
+        });
+    });
+
+    return { ...state, handleLoadChildren };
   },
-};
+});
 </script>
-
-
-<style>
-.tree{
-    padding: 20px;
-}
-</style>
