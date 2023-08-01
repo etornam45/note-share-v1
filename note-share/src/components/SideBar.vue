1f9ee28ba54cc7c72b0b8452c4e1c4b040543c91
etornam45 <navidben45@gmail.com>
2023-08-01 13:43:43 +0000

    Re-disign and implemented the side bar tree

--- note-share/src/components/SideBar.vue
+++ note-share/src/components/SideBar.vue
@@ -1,19 +1,20 @@
 <template>
-  <div style="padding: 20px 10px;">
+  <div style="margin: 0 0 0 20px; padding-top: 20px; overflow-y: hidden; height: calc(100% - 0px);">
     <tree-view
       :tree-data="myTreeData"
       @load-children="handleLoadChildren"
+      @add-child="addChild"
     ></tree-view>
   </div>
 </template>
 
 <script>
-import { defineComponent, onMounted, reactive } from "vue";
+import { defineComponent, onMounted, reactive, ref } from "vue";
 import TreeView from "./TreeView.vue";
 import PocketBase from "pocketbase";
 import { useUserStore } from "src/stores/user-store";
 import { useRouter } from "vue-router";
-
+import { useNoteStore } from "src/stores/new-note-store";
 
 export default defineComponent({
   components: {
@@ -22,7 +23,8 @@ export default defineComponent({
   setup() {
     const pb = new PocketBase("http://127.0.0.1:8090");
     const userStore = useUserStore();
-    const router = useRouter()
+    const router = useRouter();
+    const newNote = useNoteStore();
     const state = reactive({
       myTreeData: [],
     });
@@ -43,32 +45,105 @@ export default defineComponent({
               });
             }
           });
-        }).catch(err => {
-            console.log(err)
-        }).finally(() => {
-            // handle routing here
-            router.push('/'+ item.id)
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => {
+          // handle routing here
+          router.push("/" + item.id);
         });
     }
+
+    async function addChild(item) {
+      console.log(item);
+      newNote.opened = true;
+      newNote.parent = item.id;
+    }
+
+    const input = ref("");
+
+    async function saveNote() {
+      await pb
+        .collection("noteContent")
+        .create({
+          content: "",
+        })
+        .then(async (result) => {
+          console.log(result);
+          await pb.collection("noteTree").create({
+            name: input.value,
+            parent: newNote.parent,
+            content: result.id,
+            user: userStore.user.id,
+            sharedUsers: [],
+          });
+        });
+    }
+
+    function arrangeInHierarchy(inputArray) {
+      const tree = {};
+
+      // Create a map to quickly find nodes by their ID
+      const nodeMap = inputArray.reduce((acc, node) => {
+        acc[node.id] = node;
+        return acc;
+      }, {});
+
+      // Function to recursively build the tree
+      const buildTree = (node) => {
+        const children = inputArray.filter(
+          (childNode) => childNode.parent === node.id
+        );
+
+        if (children.length > 0) {
+          node.children = children.map((child) => buildTree(child));
+        }
+
+        return node;
+      };
+
+      // Find and build root nodes
+      const rootNodes = inputArray.filter((node) => !node.parent);
+      rootNodes.forEach((root) => {
+        tree[root.id] = buildTree(root);
+      });
+
+      return Object.values(tree);
+    }
+
+
+    const standByArray = ref([])
 
     onMounted(async () => {
       await pb
         .collection("noteTree")
         .getFullList({
-          filter: `user = '${userStore.user.id}' && parent = ''`,
+          filter: `user = '${userStore.user.id}'`,
         })
         .then((result) => {
           result.forEach((node) => {
-            state.myTreeData.push({
+            standByArray.value.push({
               label: node.name,
               id: node.id,
+              parent: node.parent, 
               children: [],
             });
           });
+        }).finally(() => {
+          // arrangeInHierarchy()
+          let value = standByArray.value
+          console.table(value);
+          let order = arrangeInHierarchy(value)
+          console.table(order);
+          
+          order.forEach(parent => {
+            state.myTreeData.push(parent)
+          })
         });
     });
 
-    return { ...state, handleLoadChildren };
+    return { ...state, handleLoadChildren, addChild, newNote, input, saveNote };
   },
 });
 </script>
--- note-share/src/components/TreeView.vue
+++ note-share/src/components/TreeView.vue
@@ -1,35 +1,66 @@
 <template>
-  <div>
+  <div class="q-tree">
     <div>
-      <div 
-        style="margin-left: 10px"
+      <div
+        class="q-tree__node relative-position q-tree__node--parent"
         v-for="item in treeData"
         :key="item.id"
       >
-        <div class="node" @click="loadChildren(item)">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#888888" d="M12 14.195c-.176 0-.348-.046-.5-.133l-9-5.198a1 1 0 0 1 0-1.732l9-5.194c.31-.177.69-.177 1 0l9 5.194a1 1 0 0 1 0 1.732l-9 5.198a1.002 1.002 0 0 1-.5.133z" opacity=".25"/><path fill="#888888" d="m21.5 11.132l-1.964-1.134l-7.036 4.064c-.31.178-.69.178-1 0L4.464 9.998L2.5 11.132a1 1 0 0 0 0 1.732l9 5.198c.31.178.69.178 1 0l9-5.198a1 1 0 0 0 0-1.732z" opacity=".5"/><path fill="#888888" d="m21.5 15.132l-1.964-1.134l-7.036 4.064c-.31.178-.69.178-1 0l-7.036-4.064L2.5 15.132a1 1 0 0 0 0 1.732l9 5.198c.31.178.69.178 1 0l9-5.198a1 1 0 0 0 0-1.732z"/></svg>
+        <div class="node q-tree__node-header relative-position row " @click="loadChildren(item)">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path
+              fill="#888888"
+              d="M12 14.195c-.176 0-.348-.046-.5-.133l-9-5.198a1 1 0 0 1 0-1.732l9-5.194c.31-.177.69-.177 1 0l9 5.194a1 1 0 0 1 0 1.732l-9 5.198a1.002 1.002 0 0 1-.5.133z"
+              opacity=".25"
+            />
+            <path
+              fill="#888888"
+              d="m21.5 11.132l-1.964-1.134l-7.036 4.064c-.31.178-.69.178-1 0L4.464 9.998L2.5 11.132a1 1 0 0 0 0 1.732l9 5.198c.31.178.69.178 1 0l9-5.198a1 1 0 0 0 0-1.732z"
+              opacity=".5"
+            />
+            <path
+              fill="#888888"
+              d="m21.5 15.132l-1.964-1.134l-7.036 4.064c-.31.178-.69.178-1 0l-7.036-4.064L2.5 15.132a1 1 0 0 0 0 1.732l9 5.198c.31.178.69.178 1 0l9-5.198a1 1 0 0 0 0-1.732z"
+            />
+          </svg>
           {{ item.label }}
 
           <div class="floating">
-            <div class="add">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#888888" d="M17 11h-4V7a1 1 0 0 0-2 0v4H7a1 1 0 0 0 0 2h4v4a1 1 0 0 0 2 0v-4h4a1 1 0 0 0 0-2Z"/><path fill="#888888" d="M21 2H3a1 1 0 0 0-1 1v18a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1Zm-4 11h-4v4a1 1 0 0 1-2 0v-4H7a1 1 0 0 1 0-2h4V7a1 1 0 0 1 2 0v4h4a1 1 0 0 1 0 2Z" opacity=".5"/></svg>
+            <div class="add" @click="addChild(item)">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><g fill="none"><path fill="#888888" d="M7 21a2 2 0 0 1-2-2V3h8v6h6v10a2 2 0 0 1-2 2H7Z" opacity=".16"/><path stroke="#888888" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a2 2 0 0 1-2-2V3h9l5 5v11a2 2 0 0 1-2 2H7Zm5-8v4m-2-2h4"/><path stroke="#888888" stroke-linejoin="round" stroke-width="2" d="M13 3v6h6"/></g></svg>
             </div>
             <div class="remove">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#888888" d="m13.414 12l3.293-3.293a1 1 0 0 0-1.414-1.414L12 10.586L8.707 7.293a1 1 0 0 0-1.414 1.414L10.586 12l-3.293 3.293a1 1 0 0 0 1.414 1.414L12 13.414l3.293 3.293a1 1 0 0 0 1.414-1.414Z"/><path fill="#888888" d="M19.07 4.93A10 10 0 1 0 4.93 19.07A10 10 0 1 0 19.07 4.93Zm-2.363 10.363a1 1 0 1 1-1.414 1.414L12 13.414l-3.293 3.293a1 1 0 0 1-1.414-1.414L10.586 12L7.293 8.707a1 1 0 0 1 1.414-1.414L12 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414L13.414 12Z" opacity=".5"/></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><g fill="none"><path fill="#888888" d="M8 21h8a2 2 0 0 0 2-2V7H6v12a2 2 0 0 0 2 2Z" opacity=".16"/><path stroke="#888888" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 7v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7M4 7h16M7 7l2-4h6l2 4"/></g></svg>
             </div>
           </div>
         </div>
-        <tree-view
-          v-if="item.children"
-          :tree-data="item.children"
-          @load-children="loadChildren"
-        ></tree-view>
+        <div v-if="newNote.opened && item.id == newNote.parent" style="display: flex; align-items: center;">
+          <input v-model="input" type="text" placeholder="name" />
+          <button @click="saveNote" style="display: inline-flex; align-items: center;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none"><path fill="#888888" d="M7 21a2 2 0 0 1-2-2V3h8v6h6v10a2 2 0 0 1-2 2H7Z" opacity=".16"/><path stroke="#888888" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a2 2 0 0 1-2-2V3h9l5 5v11a2 2 0 0 1-2 2H7Zm5-8v4m-2-2h4"/><path stroke="#888888" stroke-linejoin="round" stroke-width="2" d="M13 3v6h6"/></g></svg>
+          </button>
+        </div>
+
+        <div class="q-tree__node-collapsible">
+          <div class="q-tree__children">
+            <tree-view
+              v-if="item.children"
+              :tree-data="item.children"
+              @load-children="loadChildren"
+              @add-child="addChild"
+            ></tree-view>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { useNoteStore } from "src/stores/new-note-store";
+import PocketBase from "pocketbase";
+import { useUserStore } from "src/stores/user-store";
+const pb = new PocketBase("http://127.0.0.1:8090");
 export default {
   name: "TreeView",
   props: {
@@ -38,9 +69,41 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      newNote: useNoteStore(),
+      input: "",
+      userStore: useUserStore(),
+    };
+  },
   methods: {
     loadChildren(item) {
       this.$emit("load-children", item);
+    },
+    addChild(item) {
+      this.$emit("add-child", item);
+    },
+    async saveNote() {
+      await pb
+        .collection("noteContent")
+        .create({
+          content: "<p>Type your note here</p>",
+        })
+        .then(async (result) => {
+          console.log(result);
+          await pb
+            .collection("noteTree")
+            .create({
+              name: this.input,
+              parent: this.newNote.parent,
+              content: result.id,
+              user: this.userStore.user.id,
+              sharedUsers: [],
+            })
+            .then((result) => {
+              this.newNote.opened = false;
+            });
+        });
     },
   },
 };
@@ -48,10 +111,10 @@ export default {
 
 <style scoped>
 .node {
-  background: rgb(221, 216, 210);
+  /* background: rgb(221, 216, 210); */
   padding: 2px 18px 2px 7px;
   border-radius: 0px;
-  box-shadow: 0px 0px 2px 0px rgb(109, 108, 107);
+  /* box-shadow: 0px 0px 2px 0px rgb(109, 108, 107); */
   margin-bottom: 5px;
 
   width: max-content;
@@ -64,26 +127,31 @@ export default {
   position: relative;
 }
 
-.node:hover{
-    background: rgb(243, 238, 238);
-    box-shadow: 0 1px 1px black;
+.node:hover {
+  background: rgb(243, 238, 238);
+  box-shadow: 0 1px 1px black;
 }
 
-.node svg{
-    width: 18px;
+.node svg {
+  width: 18px;
 }
 
-.floating{
-    display: none;
-    position: absolute;
-    bottom: -100%;
-    right: 0;
-    z-index: 20;
-    align-items: center;
-    gap: 5px;
+.floating {
+  display: none;
+  position: absolute;
+  bottom: 0%;
+  right: 0;
+  z-index: 20;
+  /* align-items: center;
+  justify-content: center; */
+  gap: 5px;
+  /* background: black; */
+  padding: 5px;
+  height: 20px;
 }
 
-.node:hover > .floating{
-    display: flex;
+.node:hover > .floating {
+  display: flex;
 }
 </style>
+src/stores/new-note-store.js
