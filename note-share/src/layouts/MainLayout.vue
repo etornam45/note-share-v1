46b9e8b0df0c0c4b052f102477736b68f7a9c26b
etornam45 <navidben45@gmail.com>
2023-08-03 01:41:48 +0000

    Added notename in title bar

--- note-share/src/layouts/MainLayout.vue
+++ note-share/src/layouts/MainLayout.vue
@@ -1,42 +1,51 @@
 <script>
-import { ref } from 'vue'
-import { useUserStore } from 'src/stores/user-store'
-
-import SideBar from 'src/components/SideBar.vue'
+import { ref } from "vue";
+import { useUserStore } from "src/stores/user-store";
+import { useOpenedNote } from "src/stores/opened-note";
+import SideBar from "src/components/SideBar.vue";
+import RightSideBar from "src/components/RightSideBar.vue";
 export default {
-    setup() {
-        const leftDrawerOpen = ref(false);
-        const rightDrawerOpen = ref(false);
-        const useUser = useUserStore();
-        return {
-            leftDrawerOpen,
-            toggleLeftDrawer() {
-                leftDrawerOpen.value = !leftDrawerOpen.value;
-            },
-            useUser,
-            rightDrawerOpen,
-            toggleRightDrawer() {
-                rightDrawerOpen.value = !rightDrawerOpen.value;
-            }
-        };
-    },
-    components: { SideBar }
-}
+  setup() {
+    const openedNote = useOpenedNote();
+    const leftDrawerOpen = ref(false);
+    const rightDrawerOpen = ref(false);
+    const useUser = useUserStore();
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+      useUser,
+      rightDrawerOpen,
+      toggleRightDrawer() {
+        rightDrawerOpen.value = !rightDrawerOpen.value;
+      },
+      openedNote,
+    };
+  },
+  components: { SideBar, RightSideBar },
+};
 </script>
 
 <template>
   <q-layout view="hHh lpR fFf">
-
     <q-header reveal elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn v-if="rightDrawerOpen" dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-btn
+          v-if="rightDrawerOpen"
+          dense
+          flat
+          round
+          icon="menu"
+          @click="toggleLeftDrawer"
+        />
         <q-btn v-else dense flat round icon="close" @click="toggleLeftDrawer" />
-        
+
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
           </q-avatar>
-          {{ useUser.user?.name }}
+          {{ openedNote.note.name }}
         </q-toolbar-title>
 
         <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
@@ -44,20 +53,18 @@ export default {
     </q-header>
 
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" elevated>
-      <!-- drawer content --><side-bar/>
+      <!-- drawer content --><side-bar />
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
       <!-- drawer content -->
+      <right-side-bar />
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
 
-    <q-footer elevated class="bg-grey-8 text-white">
-    </q-footer>
-
+    <q-footer elevated class="bg-grey-8 text-white"> </q-footer>
   </q-layout>
 </template>
-
