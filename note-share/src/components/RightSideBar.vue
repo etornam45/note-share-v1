b82a9315248f17265b49d117de8c91ca25504f0f
etornam45 <navidben45@gmail.com>
2023-08-04 01:42:36 +0000

    Added Right-Side-Bar Component

---
+++ note-share/src/components/RightSideBar.vue
@@ -0,0 +1,17 @@
+<script setup>
+import User from "./User.vue";
+</script>
+
+<template>
+  <div class="container">
+    <User />
+  </div>
+  <div class="shared-with-me">
+
+  </div>
+  <div class="shared">
+    
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
